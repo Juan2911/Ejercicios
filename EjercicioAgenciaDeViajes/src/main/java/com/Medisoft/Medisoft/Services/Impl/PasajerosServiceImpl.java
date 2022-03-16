4403d354e5f9d7ffc7d383b4be3ba2c4b7a65c92
Juan Jose Guzman <juan_ratablank@hotmail.com>
2022-03-16 17:09:38 -0300

    Se modifico pasando Add abajo

--- EjercicioAgenciaDeViajes/src/main/java/com/Medisoft/Medisoft/Services/Impl/PasajerosServiceImpl.java
+++ EjercicioAgenciaDeViajes/src/main/java/com/Medisoft/Medisoft/Services/Impl/PasajerosServiceImpl.java
@@ -16,28 +16,29 @@ public class PasajerosServiceImpl implements PasajerosService {
     public List<ResultadoPasajeros> obtenerDatosDePasajeros(RequestPasajeros requestPasajeros) {
         List<Pasajeros> pasajerosDeAgencia = requestPasajeros.getPasajeros();
         List<ResultadoPasajeros> detalleDePasajero = new ArrayList<>();
+
         for (Pasajeros pasajeros : pasajerosDeAgencia) {
             ResultadoPasajeros resultadoPasajeros = new ResultadoPasajeros();
             double tarifaNino = 7500.00;
             resultadoPasajeros.setNombre(pasajeros.getNombre());
+
             if (pasajeros.getTipoTarifa() == 1) {
                 resultadoPasajeros.setDescripcionDeTarifa("Niño");
                 resultadoPasajeros.setTarifa(tarifaNino);
-                detalleDePasajero.add(resultadoPasajeros);
             }
-            if (pasajeros.getTipoTarifa() == 2) {
 
+            if (pasajeros.getTipoTarifa() == 2) {
                 resultadoPasajeros.setDescripcionDeTarifa("Adulto");
                 double valorAdulto = tarifaNino * 30 / 100;
                 resultadoPasajeros.setTarifa(tarifaNino + valorAdulto);
-                detalleDePasajero.add(resultadoPasajeros);
             }
+
             if (pasajeros.getTipoTarifa() == 3) {
                 resultadoPasajeros.setDescripcionDeTarifa("Jubilado");
                 double valorJubilado = tarifaNino * 10 / 100;
                 resultadoPasajeros.setTarifa(tarifaNino - valorJubilado);
-                detalleDePasajero.add(resultadoPasajeros);
             }
+            detalleDePasajero.add(resultadoPasajeros);
         }
 
         return detalleDePasajero;
